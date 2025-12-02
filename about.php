d1c456cc7b7dfd75e35fcba8225f660c89c11b40
Muhamad Fachri Arya Ramadhan <fachr@User1135.btn.co.id>
2025-12-02 13:31:03 +0700

    lanjutan

--- about.php
+++ about.php
@@ -32,16 +32,16 @@
       <div class="header_section header_bg">
          <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <a class="navbar-brand"href="index.html"><img src="images/logo.png"></a>
+               <a class="navbar-brand"href="index.html"><img src="images/logo b putih.png"  width="200" height="10"></a>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav ml-auto">
-                     <li class="nav-item active">
+                     <li class="nav-item ">
                         <a class="nav-link" href="index.html">Home</a>
                      </li>
-                     <li class="nav-item">
+                     <li class="nav-item active">
                         <a class="nav-link" href="about.html">About</a>
                      </li>
                      <li class="nav-item">
--- index.php
+++ index.php
@@ -46,10 +46,7 @@
                         <a class="nav-link" href="about.php">About</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="icecream.php">Products</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="services.php">Services</a>
+                        <a class="nav-link" href="products.php">Products</a>
                      </li>
                      <li class="nav-item">
                         <a class="nav-link" href="blog.php">Blog</a>
@@ -70,45 +67,45 @@
             <div class="container">
                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                   <ol class="carousel-indicators">
-                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                     <li data-target="#carouselExampleIndicators" data-slide-to="2">03</li>
+                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">1</li>
+                     <li data-target="#carouselExampleIndicators" data-slide-to="1">2</li>
+                     <li data-target="#carouselExampleIndicators" data-slide-to="2">3</li>
                   </ol>
                   <div class="carousel-inner">
                      <div class="carousel-item active">
                         <div class="row">
-                           <div class="col-sm-6">
+                           <div class="col-sm-7">
                               <!--Slide 1-->
                               <h1 class="banner_taital"> Nusantara Glow Series</h1><br><h2>Facial Cleanser Kunyit<br>Hydrating Toner Temulawak<br>Serum Anti-Aging Bunga Lawang</h2>
-                              <p class="banner_text">Fokus pada pencerahan, anti-aging, dan hidrasi menggunakan ekstrak herbal khas Indonesia</p>
-                              <div class="started_text"><a href="#">Details</a></div>
+                              <p class="banner_text"><h5>Fokus pada pencerahan, anti-aging, dan hidrasi menggunakan ekstrak herbal khas Indonesia</h5></p>
+                         <!--<div class="started_text"><a href="#">Details</a></div>-->
                            </div>
-                           <div class="col-sm-6">
-                              <div class="banner_img"><img src="images/produk1.png" width="400" height="500"></div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="carousel-item">
-                        <div class="row">
-                           <div class="col-sm-6">
-                              <h1 class="banner_taital">Ice Cream</h1>
-                              <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
-                              <div class="started_text"><a href="#">Order Now</a></div>
-                           </div>
-                           <div class="col-sm-6">
-                              <div class="banner_img"><img src="images/banner-img.png"></div>
+                           <div class="col-sm-5">
+                              <div class="banner_img"><img src="images/produk1.png" width="350" height="500"></div>
                            </div>
                         </div>
                      </div>
                      <div class="carousel-item">
                         <div class="row">
-                           <div class="col-sm-6">
-                              <h1 class="banner_taital">Ice Cream</h1>
-                              <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
-                              <div class="started_text"><a href="#">Order Now</a></div>
+                           <div class="col-sm-7">
+                              <h1 class="banner_taital"> Jamu Spa Series</h1><br><h2>Lulur Rempah Kopi<br>Body Butter Cendana<br>Sabun Mandi Herbal Sereh</h2>
+                              <p class="banner_text"><h5>Mengangkat ritual spa tradisional Indonesia ke dalam rutinitas harian.</h5></p>
+                         <!--<div class="started_text"><a href="#">Order Now</a></div>-->
                            </div>
-                           <div class="col-sm-6">
-                              <div class="banner_img"><img src="images/banner-img.png"></div>
+                           <div class="col-sm-5">
+                              <div class="banner_img"><img src="images/produk 2.png" width="350" height="500"></div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="carousel-item">
+                        <div class="row">
+                           <div class="col-sm-7">
+                              <h1 class="banner_taital"> Warna Bumi Series</h1><br><h2>Matte Lipstick Merah Senja<br>Lip Tint Merah Jambu Pagi</h2>
+                              <p class="banner_text"><h5>Mengangkat ritual spa tradisional Indonesia ke dalam rutinitas harian.</h5></p><br>
+                              <!--<div class="started_text"><a href="#">Order Now</a></div>-->
+                           </div>
+                           <div class="col-sm-5">
+                              <div class="banner_img"><img src="images/produk 3.png" width="350" height="500"></div>
                            </div>
                         </div>
                      </div>
@@ -124,121 +121,54 @@
          <div class="container">
             <div class="row">
                <div class="col-md-6">
-                  <div class="about_img"><img src="images/about-img.png"></div>
+                  <div class="about_img"><img src="images/logo 3.png"></div>
                </div>
                <div class="col-md-6">
-                  <h1 class="about_taital">About Icecream</h1>
-                  <p class="about_text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore euconsectetur adipiscing esequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
-                  <div class="read_bt_1"><a href="#">Read More</a></div>
+                  <h1 class="about_taital">Puspa Ayu</h1>
+                  <p class="about_text">Puspa Ayu adalah merek kosmetik lokal (UMKM yang telah berkembang menjadi PT) yang berdedikasi untuk menghadirkan produk kecantikan alami dan Halal dengan memanfaatkan kekayaan alam dan warisan budaya Indonesia. Didirikan dengan keyakinan bahwa kecantikan sejati berasal dari alam, Puspa Ayu berkomitmen untuk menggunakan bahan-bahan terbaik dari Nusantara, diproses dengan teknologi modern namun tetap menjunjung tinggi kearifan lokal.</p>
+                  <div class="read_bt_1"><a href="about.php">Read More</a></div>
                </div>
             </div>
          </div>
       </div>
       <!-- about sectuion end -->
-      <!-- cream sectuion start -->
+      <!-- best seller products sectuion start -->
       <div class="cream_section layout_padding">
          <div class="container">
             <div class="row">
                <div class="col-md-12">
-                  <h1 class="cream_taital">Our Featured Ice Cream</h1>
-                  <p class="cream_text">tempor incididunt ut labore et dolore magna aliqua</p>
+                  <h1 class="cream_taital">Best Seller Products</h1>
                </div>
             </div>
             <div class="cream_section_2">
                <div class="row">
                   <div class="col-md-4">
                      <div class="cream_box">
-                        <div class="cream_img"><img src="images/img-1.png"></div>
-                        <div class="price_text">$10</div>
-                        <h6 class="strawberry_text">Strawberry Ice Cream</h6>
-                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
+                        <div class="cream_img"><img src="images/facial kunyit.png"></div>
+                        <h6 class="strawberry_text">Facial Cleanser Kunyit</h6>
+                        <div class="cart_bt"><a href="#">Details</a></div>
                      </div>
                   </div>
                   <div class="col-md-4">
                      <div class="cream_box">
-                        <div class="cream_img"><img src="images/img-2.png"></div>
-                        <div class="price_text">$10</div>
-                        <h6 class="strawberry_text">Strawberry Ice Cream</h6>
-                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
+                        <div class="cream_img"><img src="images/lulur kopi.png"></div>
+                        <h6 class="strawberry_text">Lulur Rempah Kopi</h6>
+                        <div class="cart_bt"><a href="#">Details</a></div>
                      </div>
                   </div>
                   <div class="col-md-4">
                      <div class="cream_box">
-                        <div class="cream_img"><img src="images/img-1.png"></div>
-                        <div class="price_text">$10</div>
-                        <h6 class="strawberry_text">Strawberry Ice Cream</h6>
-                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
+                        <div class="cream_img"><img src="images/lipstik.png"></div>
+                        <h6 class="strawberry_text">Matte Lipstick Merah Senja</h6>
+                        <div class="cart_bt"><a href="#">Details</a></div>
                      </div>
                   </div>
                </div>
             </div>
-            <div class="cream_section_2">
-               <div class="row">
-                  <div class="col-md-4">
-                     <div class="cream_box">
-                        <div class="cream_img"><img src="images/img-3.png"></div>
-                        <div class="price_text">$10</div>
-                        <h6 class="strawberry_text">Strawberry Ice Cream</h6>
-                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
-                     </div>
-                  </div>
-                  <div class="col-md-4">
-                     <div class="cream_box">
-                        <div class="cream_img"><img src="images/img-4.png"></div>
-                        <div class="price_text">$10</div>
-                        <h6 class="strawberry_text">Strawberry Ice Cream</h6>
-                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
-                     </div>
-                  </div>
-                  <div class="col-md-4">
-                     <div class="cream_box">
-                        <div class="cream_img"><img src="images/img-5.png"></div>
-                        <div class="price_text">$10</div>
-                        <h6 class="strawberry_text">Strawberry Ice Cream</h6>
-                        <div class="cart_bt"><a href="#">Add To Cart</a></div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="seemore_bt"><a href="#">See More</a></div>
+            <div class="seemore_bt"><a href="products.php">See More</a></div>
          </div>
       </div>
-      <!-- cream sectuion end -->
-      <!-- services section start -->
-      <div class="services_section layout_padding">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <h1 class="services_taital">Our Ice Cream Services</h1>
-                  <p class="services_text">tempor incididunt ut labore et dolore magna aliqua</p>
-               </div>
-            </div>
-            <div class="services_section_2">
-               <div class="row">
-                  <div class="col-md-4">
-                     <div class="services_box">
-                        <h5 class="tasty_text"><span class="icon_img"><img src="images/icon-1.png"></span>Cookies Ice Cream</h5>
-                        <p class="lorem_text">commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fat </p>
-                     </div>
-                  </div>
-                  <div class="col-md-4">
-                     <div class="services_box">
-                        <h5 class="tasty_text"><span class="icon_img"><img src="images/icon-2.png"></span>Cookies Ice Cream</h5>
-                        <p class="lorem_text">commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fat </p>
-                     </div>
-                  </div>
-                  <div class="col-md-4">
-                     <div class="services_box">
-                        <h5 class="tasty_text"><span class="icon_img"><img src="images/icon-1.png"></span>Cookies Ice Cream</h5>
-                        <p class="lorem_text">commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fat </p>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="seemore_bt"><a href="#">Read More</a></div>
-         </div>
-      </div>
-      <!-- services section end -->
+      <!-- best seller product sectuion end -->
       <!-- testimonial section start -->
       <div class="testimonial_section layout_padding">
          <div class="container">
@@ -254,19 +184,16 @@
                         <div id="main_slider" class="carousel slide" data-ride="carousel">
                            <div class="carousel-inner">
                               <div class="carousel-item active">
-                                 <p class="testimonial_text">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint</p>
-                                 <h4 class="client_name">Marri Fen</h4>
-                                 <div class="client_img"><img src="images/client-img.png"></div>
+                                 <p class="testimonial_text">Sejak pakai Facial Cleanser Kunyit dan Serum Anti-Aging Bunga Lawang, kulit wajah saya yang kusam jadi cerah merata! Pembersihnya lembut banget, nggak bikin kering. Dan serumnya, wow, garis halus di bawah mata mulai tersamar. Benar-benar Glow alami seperti yang dijanjikan! Puspa Ayu sukses mengangkat kekayaan jamu ke skincare modern</p>
+                                 <h4 class="client_name">Rina S., 35 tahun</h4>
                               </div>
                               <div class="carousel-item">
-                                 <p class="testimonial_text">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint</p>
-                                 <h4 class="client_name">Marri Fen</h4>
-                                 <div class="client_img"><img src="images/client-img.png"></div>
+                                 <p class="testimonial_text">Akhirnya ketemu lipstik matte yang nggak bikin bibir pecah-pecah! Matte Lipstick Merah Senja ini warnanya bold dan elegan, cocok banget buat acara malam. Formulanya ringan, transfer minimal, dan yang paling penting, nyaman di bibir. Warna-warna dari seri 'Warna Bumi' ini memang cantik dan otentik!</p>
+                                 <h4 class="client_name">Maya P., 24 tahun</h4>
                               </div>
                               <div class="carousel-item">
-                                 <p class="testimonial_text">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint</p>
-                                 <h4 class="client_name">Marri Fen</h4>
-                                 <div class="client_img"><img src="images/client-img.png"></div>
+                                 <p class="testimonial_text">Lulur Rempah Kopi-nya super ampuh mengangkat daki dan bikin kulit halus seperti bayi. Wangi kopinya menenangkan</p>
+                                 <h4 class="client_name">Dewi K., 28 tahun</h4>
                               </div>
                            </div>
                            <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
@@ -312,26 +239,19 @@
                      <ul>
                         <li>
                            <a href="#">
-                           <span class="padding_left_10 active"><i class="fa fa-map-marker" aria-hidden="true"></i></span>Making this the first true</a>
+                           <span class="padding_left_10 active"><i class="fa fa-map-marker" aria-hidden="true"></i></span>Yogyakarta</a>
                         </li>
                         <li>
                            <a href="#">
-                           <span class="padding_left_10"><i class="fa fa-phone" aria-hidden="true"></i></span>Call : +01 1234567890
+                           <span class="padding_left_10"><i class="fa fa-phone" aria-hidden="true"></i></span>Call : +62 81299177258
                            </a>
                         </li>
                         <li>
                            <a href="#">
-                           <span class="padding_left_10"><i class="fa fa-envelope" aria-hidden="true"></i></span>Email : demo@gmail.com
+                           <span class="padding_left_10"><i class="fa fa-envelope" aria-hidden="true"></i></span>Email : Purpaayukosmetik@gmail.com
                            </a>
                         </li>
                      </ul>
-                  </div>
-                  <div class="mail_main">
-                     <h3 class="newsletter_text">Newsletter</h3>
-                     <div class="form-group">
-                        <textarea class="update_mail" placeholder="Enter Your Email" rows="5" id="comment" name="Enter Your Email"></textarea>
-                        <div class="subscribe_bt"><a href="#">Subscribe</a></div>
-                     </div>
                   </div>
                   <div class="footer_social_icon">
                      <ul>
@@ -349,7 +269,7 @@
       <!-- copyright section start -->
       <div class="copyright_section">
          <div class="container">
-            <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free Html Templates</a> Distribution by <a href="https://themewagon.com">ThemeWagon</a></p>
+            <p class="copyright_text">2025 All Rights Reserved. </p>
          </div>
       </div>
       <!-- copyright section end -->
